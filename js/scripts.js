dcdd2b98a34f6f8c02c1c5f3d5eb3dd6f383602e
Anshika <akshay@gmail.com>
2023-09-17 00:41:28 +0530

    Menu added

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,44 @@
-const lights = document.querySelectorAll('.light');
+// script.js
+const circles = document.querySelectorAll('.circle');
+const toggleButton = document.getElementById('toggleButton');
+const colorSelector = document.getElementById('colorSelector');
+const applyColorButton = document.getElementById('applyColor');
+let isAnimating = false;
+let interval;
 
 function changeIntensity() {
-    for (let i = lights.length - 1; i > 0; i--) {
-        lights[i].style.backgroundColor = lights[i - 1].style.backgroundColor;
+    for (let i = 0; i < circles.length; i++) {
+        setTimeout(() => {
+            circles[i].style.backgroundColor = 'white'; // Change the color to bright
+            setTimeout(() => {
+                circles[i].style.backgroundColor = getSelectedColor(); // Reset the circle
+            }, 600); // Adjust this timing for the glowing effect
+        }, i * 120); // Adjust this timing for the overall animation speed
     }
-    lights[0].style.backgroundColor = '#FF0000'; // Change this to the desired bright color
 }
 
-function startAnimation() {
-    setInterval(changeIntensity, 1000); // Change the interval (milliseconds) as needed
+function getSelectedColor() {
+    return colorSelector.value; // Get the selected color from the menu
 }
 
-startAnimation();
+function toggleAnimation() {
+    if (isAnimating) {
+        clearInterval(interval);
+        toggleButton.textContent = 'Start Animation';
+    } else {
+        changeIntensity(); // Run the initial animation
+        interval = setInterval(changeIntensity, 7 * 120); // 7 circles, adjust timing
+        toggleButton.textContent = 'Stop Animation';
+    }
+    isAnimating = !isAnimating;
+}
+
+function applyColor() {
+    const selectedColor = getSelectedColor();
+    for (let circle of circles) {
+        circle.style.backgroundColor = selectedColor; // Apply selected color to all circles
+    }
+}
+
+toggleButton.addEventListener('click', toggleAnimation);
+applyColorButton.addEventListener('click', applyColor);
